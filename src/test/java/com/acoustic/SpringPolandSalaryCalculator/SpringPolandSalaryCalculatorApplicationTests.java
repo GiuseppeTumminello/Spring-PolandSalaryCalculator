4db706c0d09c72e1cc7604a1a6d5dd12a1febdf0
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-06-20 11:09:37 +0200

    set up postgres sql and add pom.xml file

---
+++ src/test/java/com/acoustic/SpringPolandSalaryCalculator/SpringPolandSalaryCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acoustic.SpringPolandSalaryCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPolandSalaryCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
